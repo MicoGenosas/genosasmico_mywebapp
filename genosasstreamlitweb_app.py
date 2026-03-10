021ea6ff59feb0311c33f76f2b715ee591a4b776
Mico Genosas <2024-200074@rtu.edu.ph>
2026-03-10 17:40:54 +0800

    Update genosasstreamlitweb_app.py

--- genosasstreamlitweb_app.py
+++ genosasstreamlitweb_app.py
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 import streamlit as st
 
 st.title("My Web App Project")
 st.write("Hello Everyone! I'm Mico Genosas and this is my Streamlit application.")
 
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/MicoGenosas/genosasmico_mywebapp.git
-git push -u origin main
-
-=======
 import streamlit as st
 
 st.title("My Web App Project")
